6d297d59d202b15d7a4035c39d22ab0035f03874
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2020-10-07 19:55:56 +0800

    6. Class components

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 // Functional Component
-const About = () => {
-    const message = 'Hello Nextjs';
-    return React.createElement('div', null, 'This is a test')
+// const About = () => {
+//     const message = 'Hello Nextjs';
+//     return React.createElement('div', null, 'This is a test')
+// }
+
+// Class Component
+class About extends React.Component {
+    render() {
+
+        return (
+            <h1>Hello from class component</h1>
+        )
+    }
 }
 
 export default About
